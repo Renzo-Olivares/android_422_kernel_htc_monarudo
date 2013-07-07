5f9f83084146b84187f64b28ebe3396246ecfe3e
boss <boss@boss-Inspiron-1545.(none)>
2013-07-07 12:50:26 -0700

    fix build

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -629,9 +629,7 @@ static struct android_usb_function rmnet_function = {
 	.performance_lock = 1,
 };
 
-#if 0
 #define MAX_MBIM_INSTANCES 1
-
 
 static int mbim_function_init(struct android_usb_function *f,
 					 struct usb_composite_dev *cdev)
@@ -656,7 +654,6 @@ static struct android_usb_function mbim_function = {
 	.bind_config	= mbim_function_bind_config,
 	.init		= mbim_function_init,
 };
-#endif
 
 static char diag_clients[32];	    
 static char diag_string[4][32];
@@ -2034,6 +2031,7 @@ static struct android_usb_function *supported_functions[] = {
  	&rmnet_sdio_function,
  	&rmnet_smd_sdio_function,
  	&ccid_function,
+	&mbim_function,
 	NULL
 };
 
