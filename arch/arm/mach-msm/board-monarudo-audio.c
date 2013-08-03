a7c49dd6e3154f7687abe74288228bd22c60032a
Bryan Smith <zarboz@gmail.com>
2013-08-02 21:32:51 -0600

    Fixing audio
    Signed-off-by: Bryan Smith <zarboz@gmail.com>

--- arch/arm/mach-msm/board-monarudo-audio.c
+++ arch/arm/mach-msm/board-monarudo-audio.c
@@ -14,27 +14,30 @@
  */
 
 #include <linux/platform_device.h>
-#include <mach/htc_acoustic_8960.h>
 #include <sound/pcm.h>
 #include <sound/q6asm.h>
+#include <mach/htc_acoustic_8960.h>
 #include <linux/module.h>
-#include <linux/gpio.h>
 #include "board-monarudo.h"
+#include "../sound/soc/msm/msm-pcm-routing.h"
+
+#include <linux/gpio.h>
 #include <mach/tpa6185.h>
 #include <mach/rt5501.h>
-#include "../sound/soc/msm/msm-pcm-routing.h"
-#include "../sound/soc/msm/msm-compr-q6.h"
-#define RCV_PAMP_GPIO 67
-
 static atomic_t q6_effect_mode = ATOMIC_INIT(-1);
+#define HAC_PAMP_GPIO	6
 extern unsigned int system_rev;
-extern unsigned int engineerid;
 
 static int monarudo_get_hw_component(void)
 {
     int hw_com = 0;
 
-    hw_com |= HTC_AUDIO_RT5501;
+    if(query_tpa6185())
+        hw_com |= HTC_AUDIO_TPA6185;
+
+    if(query_rt5501())
+        hw_com |= HTC_AUDIO_RT5501;
+
     return hw_com;
 }
 
@@ -59,15 +62,10 @@ int apq8064_get_q6_effect_mode(void)
 	return mode;
 }
 
-int apq8064_get_24b_audio(void)
-{
-	return 1;
-}
-
 static struct acoustic_ops acoustic = {
         .enable_digital_mic = monarudo_enable_digital_mic,
         .get_hw_component = monarudo_get_hw_component,
-	.set_q6_effect = apq8064_set_q6_effect_mode
+	.set_q6_effect = apq8064_set_q6_effect_mode,
 };
 
 static struct q6asm_ops qops = {
@@ -78,23 +76,26 @@ static struct msm_pcm_routing_ops rops = {
 	.get_q6_effect = apq8064_get_q6_effect_mode,
 };
 
-static struct msm_compr_q6_ops cops = {
-	.get_24b_audio = apq8064_get_24b_audio,
-};
-
-
 static int __init monarudo_audio_init(void)
 {
         int ret = 0;
 
-	
-	gpio_request(RCV_PAMP_GPIO, "AUDIO_RCV_AMP");
-	gpio_tlmm_config(GPIO_CFG(67, 0, GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA), GPIO_CFG_DISABLE);
+	static uint32_t audio_i2s_table[] = {
+		GPIO_CFG(35, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+		GPIO_CFG(36, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+		GPIO_CFG(37, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA),
+	};
+	pr_info("%s", __func__);
+	gpio_request(HAC_PAMP_GPIO, "AUDIO_HAC_AMP");
+	gpio_direction_output(HAC_PAMP_GPIO, 0);
+	gpio_free(HAC_PAMP_GPIO);
+	gpio_tlmm_config(audio_i2s_table[0], GPIO_CFG_DISABLE);
+	gpio_tlmm_config(audio_i2s_table[1], GPIO_CFG_DISABLE);
+	gpio_tlmm_config(audio_i2s_table[2], GPIO_CFG_DISABLE);
+
 	htc_register_q6asm_ops(&qops);
 	htc_register_pcm_routing_ops(&rops);
-	htc_register_compr_q6_ops(&cops);
 	acoustic_register_ops(&acoustic);
-	pr_info("%s", __func__);
 	return ret;
 
 }
